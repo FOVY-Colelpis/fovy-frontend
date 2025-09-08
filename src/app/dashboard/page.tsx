ed1c742850eed7badb39ddceb9adb254b6544cd2
zihui <zihui_0411@hotmail.com>
2025-09-08 17:22:03 +0800

    modify global.css

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,47 @@
+export default function Dashboard() {
+
+  return (
+    <div className="pt-[50px] min-h-screen flex">
+      <div className="flex-1 flex flex-col gap-4 p-2">
+        <SkillMapOverview/>
+        <WeeklyAdvantage/>
+      </div>
+      <div className="flex-1 flex flex-col gap-4 p-2">
+        <Growth/>
+        <Match/>
+      </div>
+    </div>
+  )
+}
+
+function SkillMapOverview() {
+  return (
+    <div className="bg-gray-300 h-[40vh]">
+      asd
+    </div>
+  )
+}
+
+function WeeklyAdvantage() {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+function Growth() {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+function Match() {
+  return (
+    <div>asd
+
+    </div>
+  )
+}
